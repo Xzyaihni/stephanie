46687152b6f879b2b024b355b7af6f0979868113
Xzyaihni <Fxlictx996@gmail.com>
2023-11-10 13:47:02 +0800

    shaders r back

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
-use yanyaengine::App;
+use yanyaengine::{App, ShadersInfo, ShaderItem};
 
 pub use app::{common, server, client};
 
 mod app;
 
+
+mod default_vertex
+{
+    vulkano_shaders::shader!
+    {
+        ty: "vertex",
+        path: "shaders/default.vert"
+    }
+}
+
+mod default_fragment
+{
+    vulkano_shaders::shader!
+    {
+        ty: "fragment",
+        path: "shaders/default.frag"
+    }
+}
 
 fn main()
 {
@@ -11,6 +29,11 @@ fn main()
         .with_title("stey funy")
         .with_textures_path("textures/normal")
         .with_icon("icon.png")
+        // i genuinely cant think of a less cancer way of taking shaders
+        .with_shaders([ShadersInfo::new(
+            ShaderItem::new(Box::new(|device| default_vertex::load(device))),
+            ShaderItem::new(Box::new(|device| default_fragment::load(device)))
+        )])
         .with_clear_color([0.0, 0.0, 0.0])
         .run();
 }
