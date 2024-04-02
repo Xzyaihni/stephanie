6f6c66be1b4b4b8f7dcf0dae04109b8c9c1c7326
Xzyaihni <Fxlictx996@gmail.com>
2024-04-03 04:53:12 +0800

    intersection gives me headaches

--- world_generation/chunks/road_intersection.scm
+++ world_generation/chunks/road_intersection.scm
@@ -1,1 +1,9 @@
-(filled-chunk (tile (quote asphalt)))
+(fill-area
+    (fill-area
+        (filled-chunk (tile (quote concrete)))
+        (make-point 2 0)
+        (make-point (- size-x 4) size-y)
+        (tile (quote asphalt)))
+    (make-point 0 2)
+    (make-point size-x (- size-y 4))
+    (tile (quote asphalt)))
