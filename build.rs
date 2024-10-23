74912c0452e57618e27dd86011d87f72101734f9
Xzyaihni <Fxlictx996@gmail.com>
2024-10-24 07:55:15 +0800

    removed useless build script

--- build.rs
+++
@@ -1,7 +0,0 @@
-use std::fs;
-
-fn main()
-{
-	let _ = fs::remove_dir_all("worlds");
-	println!("cargo:rerun-if-changed=src");
-}
--- run_normal.sh
+++ run_normal.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 cargo b
+rm -r ./worlds
 RUST_BACKTRACE=1 ./target/debug/stephanie --debug --port 12345 --name yandere
