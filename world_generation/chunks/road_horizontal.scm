55f031c5563334ffc42fa2663d623c33405659e7
Xzyaihni <Fxlictx996@gmail.com>
2025-08-03 17:24:57 +0800

    little lines on the roads

--- world_generation/chunks/road_horizontal.scm
+++ world_generation/chunks/road_horizontal.scm
@@ -1,6 +1,11 @@
 (fill-area
-    (filled-chunk (tile 'concrete))
+    (fill-area
+	(filled-chunk (tile 'concrete))
+	(make-area
+	    (make-point 0 2)
+	    (make-point size-x (- size-y 4)))
+	(tile 'asphalt))
     (make-area
-        (make-point 0 2)
-        (make-point size-x (- size-y 4)))
-    (tile 'asphalt))
+        (make-point 0 (- (/ size-y 2) 1))
+        (make-point size-x 2))
+    (tile 'asphalt-line-horizontal))
--- world_generation/chunks/road_vertical.scm
+++ world_generation/chunks/road_vertical.scm
@@ -1,6 +1,11 @@
 (fill-area
-    (filled-chunk (tile 'concrete))
+    (fill-area
+	(filled-chunk (tile 'concrete))
+	(make-area
+	    (make-point 2 0)
+	    (make-point (- size-x 4) size-y))
+	(tile 'asphalt))
     (make-area
-        (make-point 2 0)
-        (make-point (- size-x 4) size-y))
-    (tile 'asphalt))
+        (make-point (- (/ size-x 2) 1) 0)
+        (make-point 2 size-y))
+    (tile 'asphalt-line-vertical))
